74eeda13b68b86c5c048f74f25997955072eb31f
jaunify <jaeeun.lee.4h@gmail.com>
2025-05-09 11:36:19 +0900

    MS를 믿지 마시오

--- Grains/AccountGrain.cs
+++ Grains/AccountGrain.cs
@@ -2,10 +2,9 @@ using Orleans.Concurrency;
 using Orleans.Transactions.Abstractions;
 
 [GenerateSerializer]
-public record class Balance
+public class Balance
 {
-    [Id(0)]
-    public int Value { get; init; } = 1000;
+    [Id(0)] public int Value { get; set; } = 1000;
 }
 
 [Reentrant]
@@ -13,14 +12,18 @@ public class AccountGrain : Grain, IAccountGrain
 {
     private readonly ITransactionalState<Balance> _balance;
 
-    public AccountGrain(
-        [TransactionalState("balance")] ITransactionalState<Balance> balance) =>
+    public AccountGrain([TransactionalState("balance")] ITransactionalState<Balance> balance)
+    {
         _balance = balance ?? throw new ArgumentNullException(nameof(balance));
-    
-    public Task Deposit(int amount) =>
-        _balance.PerformUpdate(
-            balance => balance with { Value = balance.Value + amount });
-    
-    public Task<int> GetBalance() =>
-        _balance.PerformRead(balance => balance.Value);
+    }
+
+    public async Task Deposit(int amount)
+    {
+        await _balance.PerformUpdate(b => b.Value += amount);
+    }
+
+    public async Task<int> GetBalance()
+    {
+        return await _balance.PerformRead(balance => balance.Value);
+    }
 }
